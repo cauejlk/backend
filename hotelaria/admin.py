72b1bb76b27c1c486625d3fa3bb84fcbea86865c
IanMV <ianmatheusv54@gmail.com>
2025-06-23 11:53:32 -0300

    FEAT: Importando classes na admin.py

--- hotelaria/admin.py
+++ hotelaria/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+   Hospede,
+   Quartos,
+   Cargos,
+   Funcionarios,
+   Reserva,
+   Servicos,
+   Reserva_Servicos,
+   Turno,
+   Tipo_quarto
+)
+
+
+admin.site.register(Hospede)
+admin.site.register(Tipo_quarto)
+admin.site.register(Turno)
+admin.site.register(Cargos)
+admin.site.register(Quartos)
+admin.site.register(Funcionarios)
+admin.site.register(Reserva)
+admin.site.register(Servicos)
+admin.site.register(Reserva_Servicos)
+
